94c7825b9cd6ed918b9d892221fa3f8e9ca5714f
kamath7 <adithyakamath96@gmail.com>
2021-04-26 22:30:31 +0530

    Basic

--- Basics/app.py
+++ Basics/app.py
@@ -1,1 +1,22 @@
-print("Starting with Automate the Boring Stuff")
+print(2+2) # 2+2 -> Expression
+'''
+Order of operations
+
+a) Parantheses
+b) Division and then multiplication
+c) Addition and then subtraction
+'''
+
+print((2*3)+ (3/2) * 7)
+
+#String addition
+print('Adithya ' + 'Kamath')
+print('Adithya ' * 3)
+print('Adithya'+'!' * 10)
+
+#Variable
+spam1 = "Hello"
+spam1 = spam1 +" World"
+print(spam1)
+
+#Evaluates -> Expression. else it is a statement
