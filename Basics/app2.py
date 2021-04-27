f9c8134245650a6319df7c50858fc2b7413cd519
kamath7 <adithyakamath96@gmail.com>
2021-04-27 23:03:46 +0530

    ifs and elifs added

---
+++ Basics/app2.py
@@ -0,0 +1,44 @@
+print(True)
+
+#Comparator operators
+print (42 == 42)
+print(42 == 41)
+print (42 < 100)
+print( 100 < 98)
+
+myAge = 69
+if (myAge > 60):
+    print("Elite")
+
+print(True and True)
+print(True and False)
+
+print(True or False)
+print(True or True)
+print(False or False)
+
+print(not False)
+myName = 'Kams'
+
+print(myAge > 60 and myName == 'Kams')
+
+#Conditionals
+game = 'Hitman'
+
+if(game == 'Hitman'):
+    print('Best game')
+
+#if and else
+if (myName == "Lalle"):
+    print("Elite")
+else:
+    print("Not elite")
+
+myage1 = 27
+ 
+if myage1 ==25:
+    print("Not eligible")
+elif myage1 == 31:
+    print("Eligible")
+else:
+    print("Not comprehensible")
