23ff89f8e10d38af5ebc0cb51d2fc031970a91f1
kamath7 <adithyakamath96@gmail.com>
2021-04-30 01:16:14 +0530

    Lists

---
+++ Basics/app8.py
@@ -0,0 +1,19 @@
+#Lists
+
+sample_list = ['adithya','ronaldo','rooney','messi']
+print(sample_list)
+
+cities = [['bangalore','indore'],['tel aviv','jerusalem']]
+print(cities[0][0])
+
+print(cities[-1])
+
+lalle_arr = ['scorpion','subzero','raiden','tanya','sonya','lui kang']
+print(lalle_arr[1:3])
+print(lalle_arr[:3])
+del lalle_arr[1]
+print(lalle_arr)
+print ('scorpion' in lalle_arr)
+lalle_arr1 = ['kano','Quan Chi', 'Shinnok','Goro']
+lalle_mega = lalle_arr + lalle_arr1
+print(lalle_mega)
