c8214ccee01fd7cc094b4bff2825992b1fa89eb6
kamath7 <adithyakamath96@gmail.com>
2021-05-05 23:32:45 +0530

    String concatentation using string formatting

---
+++ Basics/app16.py
@@ -0,0 +1,11 @@
+lalle1 = 'Kams'
+lalle2 = 'is'
+lalle3 = 'the god'
+lalle4 = 'of'
+lalle5 = 'petting dogs'
+
+print(lalle1+ ' '+lalle2+' '+lalle3+' '+lalle4+' '+lalle5+'.')
+
+#String formatting
+
+print('%s %s %s %s %s.' %(lalle1, lalle2, lalle3, lalle4, lalle5))
