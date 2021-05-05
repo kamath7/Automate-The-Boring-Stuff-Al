7d862d624059fd1275677187f8fe0da341df4377
kamath7 <adithyakamath96@gmail.com>
2021-05-05 14:15:46 +0530

    Added string topic

---
+++ Basics/app14.py
@@ -0,0 +1,14 @@
+#Advanced String
+
+print('Hello It\'s Adithya') #Adding ' in between ''
+print('Hello \t It\'s Adithya \nMy age is 25')
+#Using raw string
+print(r'My favorite game is Uncharted\'s')
+print(""" My name is Adithya
+        I am aged 25
+        My hobbies are playing football""")
+my_str = 'the quick brown fox jumped over the lazy dog'
+print(my_str[0])
+print('the' in my_str)
+print('x' in my_str)
+print(my_str[0:19])
