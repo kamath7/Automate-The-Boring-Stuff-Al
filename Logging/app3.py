7663f297b74a32d10055e1de487a201415c0f2ba
kamath7 <adithyakamath96@gmail.com>
2021-05-20 01:46:19 +0530

    Stepover and breakpoint. Done on VS Code so no code sample

---
+++ Logging/app3.py
@@ -0,0 +1,7 @@
+print('Enter your name')
+name = input()
+print('Enter your age')
+age = input()
+print('Your name is {0} and your age is {1}'.format(name,age,))
+
+#Checked VS Code's - debugger, stepovers
